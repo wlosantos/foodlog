bacf9ee84d0b8cb4f4815fc054b355fb93634096
Wendel Lopes <wendelopes@msn.com>
2020-11-20 13:28:44 -0300

    Add page archive and styled

---
+++ app/controllers/archives_controller.rb
@@ -0,0 +1,7 @@
+class ArchivesController < ApplicationController
+
+  def index
+    @entries = Entry.all.group_by(&:day)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   resources :entries
   root to: 'entries#index'
+
+  get 'archives/index'
+
 end
